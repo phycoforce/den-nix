ff503283e3158c791c56a5887fe0bcb639acf72f
phycoforce <15784209+phycoforce@users.noreply.github.com>
2026-06-16 19:23:03 +0100

    chore: fix ssh deprecation message

--- modules/_home/core.nix
+++ modules/_home/core.nix
@@ -34,7 +34,22 @@
     };
   };
 
-  programs.ssh.enable = true;
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    settings."*" = {
+      ForwardAgent = false;
+      AddKeysToAgent = "no";
+      Compression = false;
+      ServerAliveInterval = 0;
+      ServerAliveCountMax = 3;
+      HashKnownHosts = false;
+      UserKnownHostsFile = "~/.ssh/known_hosts";
+      ControlMaster = "no";
+      ControlPath = "~/.ssh/master-%r@%n:%p";
+      ControlPersist = "no";
+    };
+  };
 
   programs.tmux = {
     enable = true;
