9702bb8c6f03a448ef51eae8818eaba66ca7913a
phycoforce <15784209+phycoforce@users.noreply.github.com>
2026-06-17 23:22:27 +0100

    chore: nixfmts

--- modules/media.nix
+++ modules/media.nix
@@ -1,14 +1,22 @@
 { den, ... }:
 {
   den.aspects.media = {
-    includes = [ (den.batteries.unfree [ "plex-desktop" "spotify" "plexamp" ]) ];
+    includes = [
+      (den.batteries.unfree [
+        "plex-desktop"
+        "spotify"
+        "plexamp"
+      ])
+    ];
 
-    homeManager = { pkgs, ... }: {
-      home.packages = with pkgs; [
-        plex-desktop
-        plexamp
-        spotify
-      ];
-    };
+    homeManager =
+      { pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          plex-desktop
+          plexamp
+          spotify
+        ];
+      };
   };
 }
